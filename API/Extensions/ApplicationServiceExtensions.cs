a96b39f3c53bd06199232ea219477bd2d1bf0dc4
Anton Fagerholm <anton.fagerholm@gmail.com>
2021-10-20 17:12:48 +0300

    photos can be added and deleted now

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -12,8 +12,10 @@ namespace API.Extensions
     {
         public static IServiceCollection AddAplicationServices(this IServiceCollection services, IConfiguration config) 
         {
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             // Desired for the http requests
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
